e86a6f9d44fd8c2c5e72239d7451dd98afdcef98
tonynamy <tonynamy@apperz.co.kr>
2022-06-23 12:38:43 +0900

    상세 주석 추가

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -38,7 +38,8 @@ void ofApp::setup() {
 	ofSetWindowTitle("ofEscapeTheMaze"); // Set the app name on the title bar
 	ofSetFrameRate(15);
 	ofBackground(255, 255, 255);
-	// Get the window size for image loading
+	
+	// 각종 변수 초기화
 	windowWidth = ofGetWidth();
 	windowHeight = ofGetHeight();
 
@@ -66,7 +67,7 @@ void ofApp::setup() {
 	elapsedTime = 0;
 
 	//
-	// Create a menu using ofxWinMenu
+	// ofxWinMenu 추가
 	//
 
 	// A new menu object with a pointer to this class
@@ -147,6 +148,7 @@ void ofApp::appMenuFunction(string title, bool bChecked) {
 	}
 
 	if (title == "Set Maze Size") {
+		// 미로 크기 설정
 		string a = ofSystemTextBoxDialog("Size of Maze?", to_string(MAZE_HEIGHT));
 		int size = std::stoi(a);
 
@@ -154,6 +156,7 @@ void ofApp::appMenuFunction(string title, bool bChecked) {
 		isWon = false;
 		isGiveup = false;
 
+		// 메모리 해제
 		freeMemory();
 
 		MAZE_HEIGHT = size;
@@ -165,16 +168,19 @@ void ofApp::appMenuFunction(string title, bool bChecked) {
 	//
 
 	if (title == "Full screen") {
+		// 전체화면 기능
 		bFullscreen = !bFullscreen; // Not auto-checked and also used in the keyPressed function
 		doFullScreen(bFullscreen); // But als take action immediately
 	}
 
 	if (title == "Infinite Mode") {
+		// infinite mode 토글
 		infiniteMode = !infiniteMode;
 		menu->SetPopupItem("Infinite Mode", infiniteMode);
 	}
 
 	if (title == "Giveup") {
+		// 플레이 중일때만 게임 포기 가능
 		if (isPlaying) {
 			isGiveup = true;
 		}
@@ -184,6 +190,7 @@ void ofApp::appMenuFunction(string title, bool bChecked) {
 	// Help menu
 	//
 	if (title == "About") {
+		// 안내 메시지
 		ofSystemAlertDialog("ofEscapeTheMaze\n2022-1 Comsil Final Project\nMade by Sogang Univ. tonynamy@apperz.co.kr");
 	}
 
@@ -203,18 +210,18 @@ void ofApp::draw() {
 	//ofBackground(0, 0, 0, 0);
 	ofSetColor(100);
 	ofSetLineWidth(5);
-	int i, j;
 
-	// TO DO : DRAW MAZE; 
-	// add code here
-
+	// 플레이 중일 때
 	if (isPlaying) {
 
+		// 미로 벽을 그리는 부분
 		maze_size = (min(ofGetHeight(), ofGetWidth()) - MAZE_OFFSET) / max(MAZE_WIDTH, MAZE_HEIGHT);
 		lightRadius = maze_size;
 		x_offset = (ofGetWidth() - maze_size * MAZE_WIDTH) / 2;
 		y_offset = (ofGetHeight() - maze_size * MAZE_HEIGHT) / 2;
 
+
+		// 각 미로칸에 대하여 벽(선)을 그림
 		for (int i = 0; i < MAZE_HEIGHT; i++) {
 			for (int j = 0; j < MAZE_WIDTH; j++) {
 
@@ -226,10 +233,12 @@ void ofApp::draw() {
 			}
 		}
 
+		// hint가 true일 경우에는 DFS 탈출 경로를 그림
 		if (showHint) {
 			drawRoute();
 		}
 
+		// 플레이어의 위치 및 도착점을 그림
 		int cx = playerX * maze_size + maze_size / 2 + x_offset;
 		int cy = playerY * maze_size + maze_size / 2 + y_offset;
 
@@ -244,27 +253,30 @@ void ofApp::draw() {
 		ofSetColor(ofColor::red);
 		ofDrawRectangle(gx, gy, player_size, player_size);
 
+		// focusLight가 켜져있거나 게임이 종료된 경우에는 focusLight 기능 작동
 		if (isLight || isWon || isGiveup) {
 
+			// alpha값
 			fbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
 
+			// 새로운 레이어
 			fbo.begin();
 			ofClear(ofColor::black);
 
-			int radiusStepSize = 3;  // Decrease for more circles (i.e. a more opaque brush)
-			int alpha = 3;  // Increase for a more opaque brush
-			int maxOffsetDistance = 100;  // Increase for a larger spread of circles
+			int radiusStepSize = 3;
+			int alpha = 3;
+			int maxOffsetDistance = 100;
 
 			auto a = ofColor(255, 255, 255, 255);
 			auto b = ofColor(255, 255, 255, 100);
 
+			// blendmode - subtract를 이용하여 mask 구성
 			ofEnableBlendMode(OF_BLENDMODE_SUBTRACT);
 
 			for (int radius = lightRadius + lightIncrement; radius > 0; radius -= radiusStepSize) {
 
+				// 가장자리로 갈 수록 옅어지는 효과 구현
 				ofSetColor(a.lerp(b, (lightRadius + lightIncrement) / radius));
-
-				// Using the ofColor class, we will randomly select a color between orange and red
 
 				ofDrawCircle(cx, cy, radius);
 
@@ -276,20 +288,24 @@ void ofApp::draw() {
 
 			ofSetRectMode(OF_RECTMODE_CORNER);
 
+			// 레이어 적용
 			fbo.draw(0.f, 0.f);
 		}
 
 	}
 
+	// 플레이 중일때는 경과 시간 갱신
 	if (isPlaying && !isWon && !isGiveup) {
 		elapsedTime = chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - game_clock).count();
 	}
 
+	// 경과 시간 표시
 	if (isPlaying) {
 		ofSetColor(100);
 		sprintf(str, "Move : %d\nElapsed time : %.3lfs", move, elapsedTime/(double)1000);
 		myFont.drawString(str, 15, ofGetHeight() - 20 - myFont.getLineHeight());
 
+		// 무한 모드일 때는 여태까지 찾은 블럭들의 개수 표시
 		if (infiniteMode) {
 			ofSetColor(ofColor::red);
 			sprintf(str, "Infinite Mode");
@@ -299,6 +315,7 @@ void ofApp::draw() {
 		}
 	}
 
+	// 초기 안내 메시지
 	if (!isPlaying && !isWon) {
 
 		sprintf(str, "Escape the maze!\nYou win if you find red block.\nControl : Arrow Keys\nHint : h\nOn the lights: l\nAdjust light size : [ and ]\nGive up : g\nInfinite Mode : i\n\nPress n to Start New Game");
@@ -314,6 +331,7 @@ void ofApp::draw() {
 		}
 	}
 
+	// 승리 시 메시지
 	if (isWon) {
 
 		ofSetColor(ofColor::gold);
@@ -331,6 +349,7 @@ void ofApp::draw() {
 		}
 	}
 
+	// 패배 시 메시지
 	if (isGiveup) {
 
 		ofSetColor(ofColor::red);
@@ -393,10 +412,12 @@ void ofApp::keyPressed(int key) {
 		}
 	}
 
+	// 새 게임
 	if (key == 'n') {
 		loadGame();
 	}
 
+	// 플레이 중일 때만 플레이어 이동 및 각종 기능 활성화 가능
 	if (isPlaying && !isWon && !isGiveup) {
 
 		bool playerMoved = false;
@@ -421,16 +442,20 @@ void ofApp::keyPressed(int key) {
 			playerMoved = true;
 		}
 
+		// 플레이어 이동 시에 탈출 경로 갱신
+		// 게임 종료 여부 체크
 		if (playerMoved) {
 			DFS();
 			move++;
 			gameFinishCheck();
 		}
 
+		// 힌트(탈출경로) 표시 기능
 		if (key == 'h') {
 			showHint = true;
 		}
 
+		// focus light 사이즈 조절 기능
 		if (key == ']') {
 			lightIncrement++;
 		}
@@ -439,10 +464,12 @@ void ofApp::keyPressed(int key) {
 			lightIncrement--;
 		}
 
+		// focus light toggle
 		if (key == 'l') {
 			isLight = false;
 		}
 
+		// 게임 포기
 		if (key == 'g') {
 			if (isPlaying) {
 				isGiveup = true;
@@ -450,12 +477,13 @@ void ofApp::keyPressed(int key) {
 		}
 	}
 
-	// Remove or show screen info
+	// infinite 모드 토글
 	if (key == 'i') {
 		infiniteMode = !infiniteMode;
 		menu->SetPopupItem("Infinite Mode", infiniteMode);
 	}
 
+	// full screen
 	if (key == 'f') {
 		bFullscreen = !bFullscreen;
 		doFullScreen(bFullscreen);
@@ -512,12 +540,14 @@ void ofApp::dragEvent(ofDragInfo dragInfo) {
 }
 
 void ofApp::gameFinishCheck() {
-
+	// 도착지 도잘 여부 체크
 	if (playerY == goalY && playerX == goalX) {
 		blockFound++;
+		// 무한 모드 일경우 reset=false로 하여 게임 재로드
 		if (infiniteMode) {
 			loadGame(false);
 		}
+		// 그렇지 않은 경우 isWon flag true로 설정
 		else {
 			isWon = true;
 		}
@@ -525,6 +555,7 @@ void ofApp::gameFinishCheck() {
 
 }
 
+// 랜덤 숫자를 받아오는 함수
 int ofApp::getRandNum(int min, int max) {
 	thread_local std::random_device rd;
 	thread_local std::mt19937 rng(rd());
@@ -532,24 +563,34 @@ int ofApp::getRandNum(int min, int max) {
 	return uid(rng, decltype(uid)::param_type{ min,max });
 }
 
+// 랜덤 불 값을 받아오는 함수
 int ofApp::getRandBool() {
 	return getRandNum(0, 1);
 }
 
+// 미로 생성 함수
 void ofApp::createMaze(int h, int w, int* maze, pair<int, int> start) {
 
+	// 스택
 	stack<pair<int, int>> dfsStack;
+	// 방문 배열
 	vector<int> v(h*w, 0);
+	// 미방문 연결 vertex 체크를 위한 벡터
 	vector<int> m;
 
+
+	// 상하좌워 체크를 위한 배열
 	int di[4] = { 1 , -1, 0, 0 };
 	int dj[4] = { 0, 0, 1, -1 };
+	// 각 방향과 연관된 벽
 	int dw[4] = { WALL_DOWN, WALL_UP, WALL_RIGHT, WALL_LEFT };
 	int dnw[4] = { WALL_UP, WALL_DOWN, WALL_LEFT, WALL_RIGHT };
 
+	// 시작점 push 및 방문 표시
 	dfsStack.push(start);
 	v[start.first*w + start.second] = 1;
 
+	// 스택이 빌 때 까지
 	while (!dfsStack.empty()) {
 
 		auto top = dfsStack.top();
@@ -557,6 +598,7 @@ void ofApp::createMaze(int h, int w, int* maze, pair<int, int> start) {
 		int i = top.first;
 		int j = top.second;
 
+		// 미방문 연결 vertex 체크
 		for (int k = 0; k < 4; k++) {
 
 			int ni = i + di[k];
@@ -568,11 +610,14 @@ void ofApp::createMaze(int h, int w, int* maze, pair<int, int> start) {
 			m.push_back(k);
 		}
 
+		// 미방문 연결 vertex가 없다면 pop
 		if (m.empty()) {
 			dfsStack.pop();
 			continue;
 		}
 
+		// 미방문 연결 vertex가 있다면 랜덤으로 한 vertex를 선택하고
+		// stack에 push함
 		int s = getRandNum(0, m.size()-1);
 		int ni = i + di[m[s]];
 		int nj = j + dj[m[s]];
@@ -588,8 +633,11 @@ void ofApp::createMaze(int h, int w, int* maze, pair<int, int> start) {
 
 }
 
+// 게임 로드
+// reset이 false일 경우에는 게임 실행 관련 변수들은 초기화하지 않음
 void ofApp::loadGame(bool reset) {
 
+	// 각종 변수 초기화 
 	if (reset) {
 		blockFound = 0;
 		move = 0;
@@ -610,12 +658,14 @@ void ofApp::loadGame(bool reset) {
 	}
 	memset(route, 0, sizeof(int) * MAZE_HEIGHT*MAZE_WIDTH);
 
+	// 임의 시작 위치 설정
 	if (reset) {
 		int init = getRandNum(0, MAZE_HEIGHT*MAZE_WIDTH - 1);
 
 		initX = init / MAZE_WIDTH;
 		initY = init % MAZE_HEIGHT;
 	}
+	// infinite 모드인 경우 현재 플레이어의 위치가 시작위치
 	else {
 		initX = playerX;
 		initY = playerY;
@@ -624,14 +674,18 @@ void ofApp::loadGame(bool reset) {
 	playerX = initX;
 	playerY = initY;
 
+	// 미로 생성
 	createMaze(MAZE_HEIGHT, MAZE_WIDTH, maze, {initY, initX});
 
+	// 도착지 설정
 	setGoal();
 
+	// 탈출 경로 계산
 	DFS();
 
 	isPlaying = true;
 
+	// 게임 타이머 초기화
 	if (reset) {
 		game_clock = chrono::steady_clock::now();
 	}
@@ -640,23 +694,27 @@ void ofApp::loadGame(bool reset) {
 
 void ofApp::freeMemory() {
 
-	//TO DO
+	// 게임 중인 경우에만 memory free
 	if (isPlaying) {
 		free(route);
 		free(maze);
 	}
 }
 
+// 도착지 설정 함수
 void ofApp::setGoal() {
 
 	// DFS 탐색 스택
 	stack<pair<int, int>> dfsStack;
+	// 초기 위치 삽입
 	dfsStack.push({ initY, initX });
 
 	// 방문 벡터
 	vector<int> v(MAZE_HEIGHT*MAZE_WIDTH, 0);
+	// 초기 위치 방문
 	v[initY*MAZE_WIDTH + initX] = 1;
 
+	// 스택이 빌 때 까지
 	while (!dfsStack.empty()) {
 
 		auto top = dfsStack.top();
@@ -669,6 +727,8 @@ void ofApp::setGoal() {
 		int dj[4] = { 0, -1, 0, 1 };
 		int dw[4] = { WALL_UP, WALL_LEFT, WALL_DOWN, WALL_RIGHT };
 
+		// 각 방향에 대하여
+		// 미방문 연결 지점 stack에 push
 		for (int k = 0; k < 4; k++) {
 			int ni = i + di[k];
 			int nj = j + dj[k];
@@ -681,12 +741,14 @@ void ofApp::setGoal() {
 				continue;
 			}
 			
+			// 방문 배열의 값은 현재 방문 배열의 값 + 1
 			dfsStack.push({ ni, nj });
 			v[ni*MAZE_WIDTH + nj] = v[i*MAZE_WIDTH + j] + 1;
 
 		}
 	}
 
+	// 방문 배열의 값이 최대인 좌표를 도착지로 설정
 	int max_v = 0;
 	int max_x = playerX;
 	int max_y = playerY;
@@ -702,6 +764,7 @@ void ofApp::setGoal() {
 	goalX = max_x;
 }
 
+// 탈출 경로 계산 함수
 bool ofApp::DFS()
 {
 	// 탈출 경로 초기화
@@ -709,11 +772,14 @@ bool ofApp::DFS()
 		route[i] = 0;
 	}
 
+	// dfs 스택 
 	stack<pair<int, int>> dfsStack;
 	dfsStack.push({ playerY, playerX });
 
+	// 탈출 경로 스택
 	stack<pair<int, int>> escapeStack;
 
+	// dfs 스택이 빌 때 까지
 	while (!dfsStack.empty()) {
 
 		auto top = dfsStack.top();
@@ -722,18 +788,24 @@ bool ofApp::DFS()
 		int i = top.first;
 		int j = top.second;
 
+		// 방문된 정점이라면 건너뛰기
 		if (route[i * MAZE_WIDTH + j] & ROUTE_VISITED) {
 			continue;
 		}
 
+		// 탈출 경로 스택에 push
 		escapeStack.push(top);
 
+		// 방문 표시
 		route[i * MAZE_WIDTH + j] |= ROUTE_VISITED;
-
+		
+		// 도착지라면 종료
 		if (i == goalY && j == goalX) break;
 
 		int originalSize = dfsStack.size();
 
+		// 각 방향에 대하여
+		// 미방문 연결 정점을 dfs 스택에 푸시
 		int di[4] = { -1, 0, 1, 0 };
 		int dj[4] = { 0, -1, 0, 1 };
 		int dw[4] = { WALL_UP, WALL_LEFT, WALL_DOWN, WALL_RIGHT };
@@ -747,6 +819,8 @@ bool ofApp::DFS()
 			}		
 		}
 		
+		// 미방문 연결 정점이 없다면
+		// 미로의 막다른 길이므로
 		if (originalSize == dfsStack.size() && !dfsStack.empty()) {
 
 			top = dfsStack.top();
@@ -754,12 +828,14 @@ bool ofApp::DFS()
 			i = top.first;
 			j = top.second;
 
+			// 탈출 경로 스택이 빌 때 까지
 			while (!escapeStack.empty()) {
 
 				auto rTop = escapeStack.top();
 				int ri = rTop.first;
 				int rj = rTop.second;
 
+				// dfs stack의 top-1 정점과 연결된 정점을 만날 때 까지 pop
 				bool isAdjacent = false;
 				int di[4] = { 1 , -1, 0, 0 };
 				int dj[4] = { 0, 0, 1, -1 };
@@ -772,6 +848,7 @@ bool ofApp::DFS()
 					}
 				}
 
+				// dfs 스택의 top-1 정점을 만날 수 있으면 break
 				if (isAdjacent) break;
 
 				escapeStack.pop();
@@ -780,6 +857,7 @@ bool ofApp::DFS()
 		}
 	}
 
+	// 탈출 경로 스택의 정점들을 탈출 경로로 표시
 	while (!escapeStack.empty()) {
 		auto top = escapeStack.top();
 		escapeStack.pop();
@@ -793,11 +871,14 @@ bool ofApp::DFS()
 	return 1;
 }
 
+// 탈출 경로를 그리는 함수
 void ofApp::drawRoute() {
-	//TO DO 
+	// 각 칸에 대하여
 	for (int i = 0; i < MAZE_HEIGHT; i++) {
 		for (int j = 0; j < MAZE_WIDTH; j++) {
 
+			// 각 방향에 대하여
+			// 탈출 경로인 칸의 중심을 이어 탈출 경로 표시
 			int di[4] = { 1 , -1, 0, 0 };
 			int dj[4] = { 0, 0, 1, -1 };
 			int dw[4] = { WALL_DOWN, WALL_UP, WALL_RIGHT, WALL_LEFT };
